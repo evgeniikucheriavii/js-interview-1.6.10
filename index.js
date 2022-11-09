3b52352ae32816a06ee8b28688b51c4f08bc3702
Evgenii Kucheriavyi <e.kucheriavii@gmail.com>
2022-11-09 12:29:32 +0300

    Feature added

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const _ = require('lodash')
 
 function fetchGracePeriodLogicFromMapping (cardId, mapping = '') {
-    return ''
+    let result = ''
+
+    mapping = mapping.replaceAll(' ', '')
+
+    let options = mapping.split(';')
+
+    options.forEach((option) => {
+        if(option === '') return;
+
+        let opt = option.split(':')
+        let name = opt[0]
+        let vals = opt[1].split(',')
+        vals.forEach((val) => {
+            if(cardId === val) {
+                result = name
+            }
+        })
+    })
+
+    return result === '' ? null : result
 }
 
 module.exports = fetchGracePeriodLogicFromMapping
